8fc2ad494765f011f3fd4965587b44ea2a3e3034
BodaMops25 <bodabodamops@gmail.com>
2023-05-16 00:16:06 +0000

    updated server

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const http = require('http'),
 const server = http.createServer(async (req, res) => {
 
   const url = new URL('https://localhost' + req.url)
-  if(requestHandlers[url.pathname]) {
-    const [statusCode, content, contentType = 'text/plain'] = requestHandlers[url.pathname](req, res, url)
+  let resoursePath = url.pathname.replace(/^\//, '')
+
+  if(requestHandlers[resoursePath]) {
+    const [statusCode, content, contentType = 'text/plain'] = await requestHandlers[resoursePath](req, res, url)
 
     res.writeHead(statusCode, {'Content-Type': contentType})
     res.end(content)
@@ -14,24 +16,32 @@ const server = http.createServer(async (req, res) => {
     return
   }
 
-  if(req.method === 'GET ') {
-    if(url.pathname === '/') url.pathname = '/index.html'
-    else if(url.pathname.match(/\..+/) === null) url.pathname += '.html'
+  if(req.method === 'GET') {
+    if(resoursePath === '') resoursePath = 'index.html'
+    else if(resoursePath.match(/\..+/) === null) resoursePath += '.html'
 
     let statusCode = 200,
         content = ''
 
     try {
-      content = await fileSystem.readFile(url.pathname)
+      content = await fileSystem.readFile(resoursePath)
     }
     catch(err) {
-      statusCode = 500
-      content = err
+      if(err.errno === -4058 && err.code === 'ENOENT') {
+        statusCode = 404
+        content += '<h1>File not found</h1>'
+        content += '<h3>' + err.toString() + '</h3>'
+      }
+      else {
+        statusCode = 500
+        content += '<h1>Server error</h1>'
+        content += '<h3>' + err.toString() + '</h3>'
+      }
     }
   
     let contentType = 'text/plain'
   
-    switch(url.pathname.match(/\..+$/)[0]) {
+    switch(resoursePath.match(/\..+$/)[0]) {
       case '.js':
         contentType = 'text/javascript'
         break
@@ -49,10 +59,10 @@ const server = http.createServer(async (req, res) => {
 
 }).listen(PORT)
 
-const requestHandlers {
-  'test-comand' : async (req, res, url) => {
+const requestHandlers = {
+  'test-command' : async (req, res, url) => {
     try {
-      retunr [200, url + 'is ok']
+      return [200, url + ': is ok']
     }
     catch(err) {
       return [500, err.toString()]
